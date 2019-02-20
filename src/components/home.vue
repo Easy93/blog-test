e1ccd2d251fc00643e8f067ba96723beb969f657
Easy93 <www.610544150@qq.com>
2019-02-20 18:41:31 +0800

    创建项目并实现简单登录

---
+++ src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        HOME
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Home',
+    data (){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
---
+++ src/components/login.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="login">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="登录" name="login">
+        <el-form
+          :model="loginForm"
+          status-icon
+          hide-required-asterisk
+          :rules="loginRules"
+          ref="loginForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="loginForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="loginForm.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('loginForm')">提交</el-button>
+            <el-button @click="resetForm('loginForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+
+      <el-tab-pane label="注册" name="register">
+        <el-form
+          :model="registerForm"
+          status-icon
+          hide-required-asterisk
+          :rules="registerRules"
+          ref="registerForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="registerForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="pass">
+            <el-input type="password" v-model="registerForm.pass" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="checkPass">
+            <el-input type="password" v-model="registerForm.checkPass" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('registerForm')">提交</el-button>
+            <el-button @click="resetForm('registerForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Login",
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.registerForm.checkPass !== "") {
+          this.$refs.registerForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.registerForm.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      activeName: "login",
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      loginRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      },
+      registerForm: {
+        username: "",
+        pass: "",
+        checkPass: ""
+      },
+      registerRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          axios
+            .post("/apis/user/login", {
+              username: this.loginForm.username,
+              password: this.loginForm.password
+            })
+            .then(res => {
+              if (res.data.code == 1) {
+                this.$message({
+                  message: res.data.msg,
+                  type: "success"
+                });
+                this.$router.push({name:"home"})
+              } else {
+                this.$message({
+                  message: res.data.errMsg,
+                  type: "error"
+                });
+              }
+            })
+            .catch(res => {
+              this.$message({
+                message: "服务器错误",
+                type: "error"
+              });
+            });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.login {
+  padding: 50px;
+  margin: 0 auto;
+  .el-tabs {
+    width: 400px;
+    margin: 0 auto;
+    .el-input {
+      width: 300px;
+    }
+  }
+}
+</style>
