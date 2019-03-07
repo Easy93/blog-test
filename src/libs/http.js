7c7f9a6b11aa56116e93da23c290ac1804c0a834
Easy93 <www.610544150@qq.com>
2019-03-07 17:01:58 +0800

    axios封装

---
+++ src/libs/http.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const http = axios.create({
+  timeout: 1000 * 30,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json; charset=utf-8"
+  }
+});
+
+http.adornUrl = actionName => {
+  return (
+    (process.env.NODE_ENV == "production" ? process.env.ROOT : "/proxyApi/") +
+    actionName
+  );
+};
+
+export default http;
